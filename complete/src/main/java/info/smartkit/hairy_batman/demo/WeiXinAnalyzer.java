813415b38d34c54f2447e5a9d6af410377166083
hunter006 <liuzg_2000@163.com>
2015-02-02 00:14:04 +0800

    withdraw

--- complete/src/main/java/info/smartkit/hairy_batman/demo/WeiXinAnalyzer.java
+++ complete/src/main/java/info/smartkit/hairy_batman/demo/WeiXinAnalyzer.java
@@ -3,7 +3,7 @@ package info.smartkit.hairy_batman.demo;
 public class WeiXinAnalyzer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub 
+		// TODO Auto-generated method stub
 
 	}
 
